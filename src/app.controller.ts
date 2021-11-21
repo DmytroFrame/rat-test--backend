c765ea57a583f6518266baa17424fadaddbfb78e
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2021-11-21 09:37:58 +0200

    test get and create

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { retry } from 'rxjs';
 import { AppService } from './app.service';
+
+
+let a = {}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Post()
+  getHello(@Body() body) {
+    // console.log(body)
+    a = body
+    return {message: "thx for you resole"}
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  back() {
+    return a
   }
 }
